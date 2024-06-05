c3cae4fa63c9646ae8ab5774dd47df933e16c074
abhinavreddy05 <abhinavreddy05@kgpian.iitkgp.ac.in>
2024-06-06 04:25:25 +0530

    changed hosting and updated url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
     inputRef.current.value = ''
 
     try {
-      const response = await axios.get('https://dbot-cz1g.onrender.com/chat', {
+      const response = await axios.get('https://long-bird-6279.ploomberapp.io/chat', {
         params: {
           message: userInput
         }
